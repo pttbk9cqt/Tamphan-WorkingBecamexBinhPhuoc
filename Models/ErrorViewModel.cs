868a7fe83b5fc13271c9e9463d81cdccf5f4192f
pttbk <pttbk@PC-25004>
2025-12-25 14:54:19 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tamphan_WorkingBecamexBinhPhuoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
