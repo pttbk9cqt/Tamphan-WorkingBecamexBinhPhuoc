caa9b068739d61c2b71c2a5346ed63b201cd25b1
Phan Thanh Tam <pttbk9cqt@gmail.com>
2025-12-25 16:08:23 +0700

    Initial MVC project with HelloController

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tamphan_WorkingBecamexBinhPhuoc.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello MVC - commit đầu tiên!";
+            return View();
+        }
+    }
+}
