df8be599f15e6bf3039700d490c3b1281d2d5343
Pham Thanh Thuong <thanhthuong2308@gmail.com>
2026-01-03 19:59:21 +0700

    Lam o may Tuat 3.1.2025

--- Models/User.cs
+++ Models/User.cs
@@ -2,5 +2,9 @@
 {
     public class User
     {
+        public int ID { get; set; }
+        public char Name { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
