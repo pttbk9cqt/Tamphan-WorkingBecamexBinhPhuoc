ec76c7e676736196cbafd4df8e23b4b41841ce8e
Pham Thanh Thuong <thanhthuong2308@gmail.com>
2026-01-03 21:49:18 +0700

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tamphan_WorkingBecamexBinhPhuoc.Models;
 
 namespace Tamphan_WorkingBecamexBinhPhuoc.Controllers
 {
@@ -6,8 +7,29 @@ namespace Tamphan_WorkingBecamexBinhPhuoc.Controllers
     {
         public IActionResult Index()
         {
+            User u1 = new User()
+            {
+                ID = 10,
+                Name = "Tam Phan",
+                Password = "daylapass",
+                PhoneNumber = "098776654322"
+            };
+            User u2 = new User()
+            {
+                ID = 12,
+                Name = "Tam Phan 2",
+                Password = "daylapass",
+                PhoneNumber = "0987762"
+            };
+            List<User> users = new List<User>() { u1, u2 };
             ViewBag.Title = "Hello MVC - commit đầu tiên tôi tạo đó!";
-            return View();
+            ViewBag.CC = "vl";
+            //getrandom();9
+            return View(users);
+        }
+        private string getrandom()
+        {
+            return "";
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -3,8 +3,26 @@
     public class User
     {
         public int ID { get; set; }
-        public char Name { get; set; }
-        public string Password { get; set; }
-        public string PhoneNumber { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public User() { }
+        public User(int iD, string name, string password, string phoneNumber)
+        {
+            ID = iD;
+            Name = name;
+            Password = password;
+            PhoneNumber = phoneNumber;
+        }
+        public User(string password, string name)
+        {
+            Name = name;
+            Password = password;
+        }
+        public string GetPhoneNumber()
+        {
+            //vi du 0985423182, ma qg vn
+            return "+84" + PhoneNumber;
+        }
     }
 }
